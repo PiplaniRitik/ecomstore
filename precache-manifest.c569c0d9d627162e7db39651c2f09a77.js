fcbb9ecc990cb0077555e93ffb5a28f483175c52
PiplaniRitik <ritik.piplani.che19@itbhu.ac.in>
2023-10-09 15:26:04 +0530

    Updates

---
+++ precache-manifest.c569c0d9d627162e7db39651c2f09a77.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b76acca3b3d20d18545f26a289be5a1c",
+    "url": "/ecomstore/index.html"
+  },
+  {
+    "revision": "ce3bdfba092b614e23e7",
+    "url": "/ecomstore/static/js/2.1b5578f1.chunk.js"
+  },
+  {
+    "revision": "70af1bffd51a7281193fa8f9b7319597",
+    "url": "/ecomstore/static/js/2.1b5578f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5bc3763de19c79221849",
+    "url": "/ecomstore/static/js/main.b34758c3.chunk.js"
+  },
+  {
+    "revision": "4390bd9d7bc9fde9e86e",
+    "url": "/ecomstore/static/js/runtime-main.fe5901da.js"
+  }
+]);
